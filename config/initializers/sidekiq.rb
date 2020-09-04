4bb7fe79f605637ea5d5ad7e08834f9504686783
Guido Barbaglia <guido@vinomofo.com>
2020-09-04 19:02:59 +1000

    Switch to Heroku's Redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://redistogo:ea085b508bdf053c861d846d34032956@scat.redistogo.com:9588" }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://redistogo:ea085b508bdf053c861d846d34032956@scat.redistogo.com:9588" }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
